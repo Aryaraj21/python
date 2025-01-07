fb3fc682069e74a0600fd43bf2117a7ef709db8d
Aryaraj21 <aryarajjadeja2114@gmail.com>
2025-01-07 16:00:40 +0530

    Add files via upload

---
+++ 24 programes.py
@@ -0,0 +1,50 @@
+a=23
+b=17
+c=a+b
+print("sum =",c)
+s=a-b
+print("sub =",s)
+m=a*b
+print("Product=",m)
+d=a/b
+print("Division=",d)
+
+
+#hours to minutes conversion
+print("conversion of hour into min")
+h=1
+minutes=h*60
+print("total minutes",minutes)
+
+#minutes to hours conversion
+print("coversion of min into hours")
+min_s=1
+h=min_s/60
+print("total hours",h)
+
+#conversion of dollars into rupees
+dollars=float(input("Please enter dollars:"))
+rupees=dollars*48
+print(rupees,"Rupees")
+
+#conversion of rupees into dollars
+rupees=int(input("Please enter rupees:"))
+dollars=rupees/48
+
+#conversion of gram into kg
+grams = float(input("Please enter grams:"))
+kilograms=grams/1000
+print(kilograms,"kilograms")
+
+#conversion of kg into gram
+kilograms = int(input("Please enter kg:"))
+grams=kilograms*1000
+print(grams)
+
+
+
+
+
+
+
+
